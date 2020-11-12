5ce698ae2bc4d799df5b57e2e3613b7f24bd5523
Simon Hong <shong@brave.com>
2020-11-12 11:36:13 +0900

    Cleanup AvatarToolbarButton patches

--- browser/ui/views/profiles/brave_avatar_toolbar_button.cc
+++ browser/ui/views/profiles/brave_avatar_toolbar_button.cc
@@ -7,10 +7,12 @@
 
 #include "brave/app/vector_icons/vector_icons.h"
 #include "brave/browser/profiles/profile_util.h"
+#include "brave/grit/brave_generated_resources.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/profiles/avatar_toolbar_button.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/pointer/touch_ui_controller.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/image/image.h"
@@ -40,4 +42,11 @@ ui::ImageModel BraveAvatarToolbarButton::GetAvatarIcon(
         icon_color, icon_size);
   }
   return AvatarToolbarButton::GetAvatarIcon(state, gaia_account_image);
+}
+
+base::string16 BraveAvatarToolbarButton::GetAvatarTooltipText() const {
+  if (brave::IsTorProfile(browser_->profile()))
+    return l10n_util::GetStringUTF16(IDS_TOR_PROFILE_NAME);
+
+  return AvatarToolbarButton::GetAvatarTooltipText();
 }
